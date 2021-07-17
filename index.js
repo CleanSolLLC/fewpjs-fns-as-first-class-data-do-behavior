74e2216034a4548e70592584aab086e5c8740ce1
mamark90 <mamark90@gmail.com>
2021-07-17 08:05:39 -0400

    commit tree f69a8c02a41b6bde0f6445d59fd4fa4df2a68d62

--- index.js
+++ index.js
@@ -9,5 +9,24 @@ function handleClick(e) {
 
 /* End Given Code, don't edit above here...*/
 
-/* Write your implementation of greet() */
-/* Write your implementation of displayMessage() */
+function greet(input) {
+  const time = parseInt(input.split(':'));
+
+  if (time < 12) {
+    var msg = 'Good Morning';
+  } else if (time >= 12 && time < 17) {
+
+    var msg = 'Good Afternoon';
+
+  } else if (time >= 17){
+    var msg = 'Good Evening'; 
+  }
+  return msg;
+}
+
+
+  function displayMessage(string) {
+       let element = document.getElementById('greeting');
+   element.innerHTML = `${string}`;
+  }
+
